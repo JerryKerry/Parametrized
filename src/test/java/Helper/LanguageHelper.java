b79913ab330e10c36838552a117518e99d2d29dc
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-07-27 16:04:55 +0300

    Добавлен стрим для параметризированного теста

---
+++ src/test/java/Helper/LanguageHelper.java
@@ -0,0 +1,17 @@
+package Helper;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LanguageHelper {
+    static Stream<Arguments> languageDropDown() {
+        return Stream.of(
+                Arguments.of("Nederlands", List.of("VPS Hosting", "Dedicated servers","WordPress","Web Hosting",
+                       "Website Diensten")),
+                Arguments.of("Español", List.of("VPS Hosting","Servidores dedicados","WordPress","Web Hosting"
+                        ,"Servicios web"))
+        );
+    }
+}
